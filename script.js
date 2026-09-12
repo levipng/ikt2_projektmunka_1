c2c174d690d2757963e33e5339d98e188b3d8b8d
Zsombor <116731984+324fa5fa-cbd2-40a5-bc60-7978edd118d3@users.noreply.github.com>
2026-09-12 21:31:51 +0200

    password bit checker

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const jelszo = "XY7#kL9!pQ2$";
+const eredmeny = zxcvbn(jelszo);
+const bitEroseg = Math.round(Math.log2(eredmeny.guesses) * 10) / 10;
+console.log(jelszo, eredmeny, bitEroseg);
+
 userIP = '';
 getIP();
 async function getIP() {
@@ -10,3 +15,5 @@ async function getIP() {
 function doneIP(x){
   document.getElementById("userIP_text").textContent=userIP
 }
+
+
