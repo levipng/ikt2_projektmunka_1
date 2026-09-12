f8dc668465ac087a63555763b2d9c49393f6a994
Zsombor <116731984+324fa5fa-cbd2-40a5-bc60-7978edd118d3@users.noreply.github.com>
2026-09-12 22:46:43 +0200

    local js & css

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const jelszo = "XY7#kL9!pQ2$";
-const eredmeny = zxcvbn(jelszo);
-const bitEroseg = Math.round(Math.log2(eredmeny.guesses) * 10) / 10;
-console.log(jelszo, eredmeny, bitEroseg);
+//IP ################
 
 userIP = '';
 getIP();
+
 async function getIP() {
   const response = await fetch('https://api.ipify.org?format=json');
   const data = await response.json();
@@ -12,13 +10,12 @@ async function getIP() {
   console.log(userIP);
   doneIP(userIP);
 }
+
 function doneIP(x){
   document.getElementById("userIP_text").textContent=userIP;
 }
 
-function passwdBitChange(bit){
-  document.getElementById("passwdBitText").textContent=(`A jelszó bit erőssége: ${bit}`);
-}
+//PASSWD ################
 
 document.getElementById('passwdText').addEventListener('input', function(e) {
     passwdBitChange(passwdBitValue(e.target.value));
@@ -29,3 +26,8 @@ function passwdBitValue(passwd){
   const bitErosseg = Math.round(Math.log2(passwdEredmeny.guesses) * 10) / 10;
   return bitErosseg;
 }
+
+function passwdBitChange(bit){
+  document.getElementById("passwdBitText").textContent=(`A jelszó bit erőssége: ${bit}`);
+}
+
