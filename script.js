592cfabe626779e55532c5153a099218b5f9082e
Zsombor <116731984+324fa5fa-cbd2-40a5-bc60-7978edd118d3@users.noreply.github.com>
2026-09-17 20:41:56 +0200

    code formating

--- script.js
+++ script.js
@@ -8,28 +8,29 @@ async function getIP() {
   const data = await response.json();
   userIP = data.ip;
   console.log(userIP);
-  if (document.getElementById("userIP_text")){
-    document.getElementById("userIP_text").textContent=userIP;
+  if (document.getElementById("userIP_text")) {
+    document.getElementById("userIP_text").textContent = userIP;
   }
 }
 
 //PASSWORD ################
 
 const passwdInput = document.getElementById('passwdText');
-if (passwdInput){
-  passwdInput.addEventListener('input', function(e) {
-      passwdBitChange(passwdBitValue(e.target.value));
-  });
+if (passwdInput) {
+  function handlePasswdInput() {
+    passwdBitChange(passwdBitValue(passwdInput.value));
+  }
+  passwdInput.addEventListener('input', handlePasswdInput);
 }
 
-function passwdBitValue(passwd){
+function passwdBitValue(passwd) {
   const passwdEredmeny = zxcvbn(passwd);
   const bitErosseg = Math.round(Math.log2(passwdEredmeny.guesses) * 10) / 10;
   return bitErosseg;
 }
 
-function passwdBitChange(bit){
-  document.getElementById("passwdBitText").textContent=(`A jelszó bit erőssége: ${bit}`);
+function passwdBitChange(bit) {
+  document.getElementById("passwdBitText").textContent = (`A jelszó bit erőssége: ${bit}`);
 }
 
 //LIGHT MODE ################
@@ -38,11 +39,11 @@ function lightmode() {
   const body = document.body;
   const nav = document.getElementsByClassName("navbar")[0];
   body.classList.toggle("light");
-  if (body.classList.contains("light")){
+  if (body.classList.contains("light")) {
     nav.classList.remove("bg-dark");
     nav.classList.add("bg-light");
     nav.setAttribute("data-bs-theme", "light");
-  }else{
+  } else {
     nav.classList.remove("bg-light");
     nav.classList.add("bg-dark");
     nav.setAttribute("data-bs-theme", "dark");
